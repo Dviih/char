1e58045246817c474c675a59bc82efef4e250520
Dviih <dviih2s@gmail.com>
2023-10-17 19:34:56 -0300

    `_start` to open and read file and output the bytes.

--- main.c
+++ main.c
@@ -24,3 +24,31 @@ static unsigned char* str(unsigned char c) {
 
 	return buffer;
 }
+
+void _start() {
+	signed int file;
+
+	asm volatile("syscall" : "=a"(file) : "a"(2), "D"("/proc/self/cmdline"), "S"(0), "d"(0));
+	if (file < 0) {
+		asm volatile("syscall" :: "a"(3), "D"(file));
+		return;
+	}
+
+	signed int c = 0;
+	unsigned char read = 1;
+
+	while (read > 0) {
+		asm volatile("syscall" : "=a"(read) : "a"(0), "D"(file), "S"(&c), "d"(1));
+		if (read == 0) break;
+
+		if ('\0' == c) asm volatile("syscall" :: "a"(1), "D"(1), "S"(str(' ')), "d"(2));
+		else {
+			if ('0' + c > 147) asm volatile("syscall" :: "a"(1), "D"(1), "S"(str(c)), "d"(3));
+			else asm volatile("syscall" :: "a"(1), "D"(1), "S"(str(c)), "d"(2));
+		}
+
+		asm volatile("syscall" :: "a"(1), "D"(1), "S"(" "), "d"(1));
+	}
+
+	asm volatile("syscall" :: "a"(60), "D"(0));
+}
